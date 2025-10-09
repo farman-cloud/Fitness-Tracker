579159718e6455cff19af28c9e60624300b65a15
Piyush Singh <rr2rinku@gmail.com>
2025-10-09 22:15:34 +0530

    Added starting files for frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-5xl font-semibold text-gray-500">Hello Team !</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
