aa0e35a26e1696513a37a350a477f923350681f1
adi3489 <07aditya.cn@gmail.com>
2025-10-10 11:57:36 +0530

    First commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 function App() {
   return (
     <>
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-5xl font-semibold text-gray-500">Hello Team !</h1>
-    </div>
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-5xl font-semibold text-gray-500">
+          Hello Everyone !
+        </h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
